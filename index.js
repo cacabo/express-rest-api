fd36f5c797301fe942618468c3555da677e9d7e6
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-09 15:41:27 -0700

    updated index

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var express = require("express");
 var app = express();
 
+// Find the proper port to listen on
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Express server is listening on port ${port}`);
+  console.log(`Express server listening on port ${port}`);
 });
