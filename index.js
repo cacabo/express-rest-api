64105c1c59b533ed26a767562397e9c6d63d1cf0
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-09 15:49:59 -0700

    added json parser

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 var express = require("express");
 var app = express();
+var jsonParser = require('body-parser').json;
+
+app.use(jsonParser());
+
+// Middleware (this is just for experimentation)
+app.use(function(req, res, next) {
+  // Call the next middleware function
+  next();
+});
 
 // Find the proper port to listen on
 var port = process.env.PORT || 3000;
